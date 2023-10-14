97a16e3ae0033ca9be7a1d60a570cc0f8d13f69b
Betty Nakyambadde <139006497+BettyNakyambadde@users.noreply.github.com>
2023-10-14 22:14:37 +0200

    Create WherewithNon-Numeric.sql

---
+++ WherewithNon-Numeric.sql
@@ -0,0 +1,4 @@
+-- Write a query to filter the accounts table to include the company name, website, and the primary point of contact (primary_poc) just for the Exxon Mobil company in the accounts table.
+SELECT name, website, primary_poc
+FROM accounts
+WHERE name = 'Exxon Mobil'
